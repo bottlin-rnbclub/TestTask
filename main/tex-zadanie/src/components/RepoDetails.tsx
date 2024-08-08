aa3cd1cf993838d49685b2987c20c5b481c36a51
Baiter_66 <146209582+bottlin-rnbclub@users.noreply.github.com>
2024-08-08 11:15:18 +0300

    RepoDetails.tsx

--- main/tex-zadanie/src/components/RepoDetails.tsx
+++ main/tex-zadanie/src/components/RepoDetails.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import { useAppSelector } from '../store/hooks';
 import { Box, Chip, Typography } from '@mui/material';
 
-const RepoDetails: React.FC = () => {
-  const selectedRepo = useAppSelector((state) => state.repos.selectedRepo);
+const RepoDetails: React.FC = () => { // 
+  const selectedRepo = useAppSelector((state) => state.repos.selectedRepo); // кастом хук useAppSelector( callback function )
 
-  if (!selectedRepo) {
+  if (!selectedRepo) { // проверка с НЕ
     return (
-      <Box sx={{ padding: '20px' }}>
+      <Box sx={{ padding: '20px' }}> // styles
         <Typography variant="h6">Выберите репозиторий</Typography>
       </Box>
     );
   }
 
   return (
-    <Box sx={{ padding: '20px', borderLeft: '1px solid #e0e0e0', height: '100vh' }}>
-      <Typography variant="h4" sx={{ marginBottom: '20px' }}>{selectedRepo.name}</Typography>
+    <Box sx={{ padding: '20px', borderLeft: '1px solid #e0e0e0', height: '100vh' }}> // styles
+      <Typography variant="h4" sx={{ marginBottom: '20px' }}>{selectedRepo.name}</Typography> 
       <Typography variant="body1" sx={{ marginBottom: '10px' }}>{selectedRepo.description}</Typography>
       {selectedRepo.language && (
         <Chip label={selectedRepo.language} sx={{ marginRight: '5px' }} />
       )}
-      <Typography variant="body2" sx={{ marginTop: '20px' }}>License: {selectedRepo.license?.name || 'No license'}</Typography>
+      <Typography variant="body2" sx={{ marginTop: '20px' }}>License: {selectedRepo.license?.name || 'No license'}</Typography> 
     </Box>
   );
 };
