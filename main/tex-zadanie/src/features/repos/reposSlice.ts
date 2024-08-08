dd35b0c7b5d6a9797a2970c82af89147786bd3f1
Baiter_66 <146209582+bottlin-rnbclub@users.noreply.github.com>
2024-08-08 11:34:05 +0300

    reposSlice.ts

--- main/tex-zadanie/src/features/repos/reposSlice.ts
+++ main/tex-zadanie/src/features/repos/reposSlice.ts
@@ -20,8 +20,8 @@ export const fetchRepos = createAsyncThunk<Repo[], string>( // асинх фун
   }
 );
 
-export const fetchRepoDetails = createAsyncThunk<Repo, { owner: string; repo: string }>(
-  'repos/fetchRepoDetails',
+export const fetchRepoDetails = createAsyncThunk<Repo, { owner: string; repo: string }>( // асинх функция для получения 2 параметров, автор и репозиторий
+  'repos/fetchRepoDetails', // его обработка с помощью try catch
   async ({ owner, repo }, { rejectWithValue }) => {
     try {
       const data = await getRepoDetails(owner, repo);
@@ -32,23 +32,23 @@ export const fetchRepoDetails = createAsyncThunk<Repo, { owner: string; repo: st
   }
 );                                        
                                                               // редьюсер
-const reposSlice = createSlice({ 
+const reposSlice = createSlice({ // срез ( slice ) для управление состоянием репос 
   name: 'repos',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {
+  extraReducers: (builder) => {  //аргумент билдер - обработка асинх действий
     builder
-      .addCase(fetchRepos.pending, (state) => {
-        state.status = 'loading';
+      .addCase(fetchRepos.pending, (state) => { // Обработчик для действия `fetchRepos.pending` - вызывается когда он запущен
+        state.status = 'loading'; // cтатус загрузки
       })
-      .addCase(fetchRepos.fulfilled, (state, action: PayloadAction<Repo[]>) => {
+      .addCase(fetchRepos.fulfilled, (state, action: PayloadAction<Repo[]>) => { // вызод когда он завершен
         state.status = 'idle';
         state.repos = action.payload;
       })
-      .addCase(fetchRepos.rejected, (state) => {
+      .addCase(fetchRepos.rejected, (state) => { // вызов ошибки
         state.status = 'failed';
       })
-      .addCase(fetchRepoDetails.fulfilled, (state, action: PayloadAction<Repo>) => {
+      .addCase(fetchRepoDetails.fulfilled, (state, action: PayloadAction<Repo>) => { // вызов непосредственно во время процесса action
         state.selectedRepo = action.payload;
       });
   },
