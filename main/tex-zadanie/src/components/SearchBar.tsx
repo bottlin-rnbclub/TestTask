68e2e9d35be855edf343751c834e219881658fc7
Baiter_66 <146209582+bottlin-rnbclub@users.noreply.github.com>
2024-08-08 11:23:59 +0300

    SearchBar.tsx

--- main/tex-zadanie/src/components/SearchBar.tsx
+++ main/tex-zadanie/src/components/SearchBar.tsx
@@ -4,26 +4,26 @@ import { fetchRepos } from '../features/repos/reposSlice';
 import { TextField, Button, Box } from '@mui/material';
 
 const SearchBar: React.FC = () => {
-  const [query, setQuery] = useState('');
-  const dispatch = useAppDispatch();
+  const [query, setQuery] = useState(''); //  реализация поисковой строки
+  const dispatch = useAppDispatch(); // отправляет запрос на репозиторий по запросу user
 
   const handleSearch = () => {
-    if (query) {
+    if (query) { // очередная проверка и получение query
       dispatch(fetchRepos(query));
     }
   };
 
   return (
-    <Box sx={{ display: 'flex', alignItems: 'center', backgroundColor: '#00838F', padding: '30px', margin: '0px'}}>
+    <Box sx={{ display: 'flex', alignItems: 'center', backgroundColor: '#00838F', padding: '30px', margin: '0px'}}> // styles
       <TextField
         label="Введите поисковый запрос"
         variant="outlined"
         fullWidth
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        sx={{ backgroundColor: 'white', borderRadius: '4px', marginRight: '10px' }}
+        onChange={(e) => setQuery(e.target.value)} // Обработчик событий
+        sx={{ backgroundColor: 'white', borderRadius: '4px', marginRight: '10px' }} // styles
       />
-      <Button className='btn handle-Search' variant="contained" color="primary" onClick={handleSearch}>
+      <Button className='btn handle-Search' variant="contained" color="primary" onClick={handleSearch}> // кнопка с функцией handleSearch
         Искать
       </Button>
     </Box>
